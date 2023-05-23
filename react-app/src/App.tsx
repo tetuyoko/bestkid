c740c355654afc3f859b1e6d09b542636db85367
tetuyoko <tyokoyama53@gmail.com>
2023-05-23 19:15:41 +0900

    feat: Add AppoloClient

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+//import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client'
 
-function App() {
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+const DisplayLocations = () => {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return data.locations.map(({ id, name, description, photo } : any ) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +40,7 @@ function App() {
         <p>
           This is shit.
         </p>
+        <DisplayLocations />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 import reportWebVitals from './reportWebVitals';
+
+console.log('--------------------------------')
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+ </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
